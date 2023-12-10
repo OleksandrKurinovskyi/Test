638e95d81bba0e5849f9a17da5ccf03b668bd327
Oleksandr Kurinovskyi <o.kurinovskyi@gmail.com>
2023-12-10 10:53:20 +0200

    test commit 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,9 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine( "Test" );
+            Console.WriteLine( "Test #2" );
         }
     }
 }
