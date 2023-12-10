a77f5825c8308296d24b462284da56c93d15f2dc
Test <o.kurinovskyi@gmail.com>
2023-12-10 10:40:13 +0200

    test commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine( "Test" );
         }
     }
 }
